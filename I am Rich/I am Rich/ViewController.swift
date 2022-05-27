e9ae151eb46bb733111b2a4fe93d2ba0bdd701d7
normilkyway <104739124+normilkyway@users.noreply.github.com>
2022-05-26 21:53:00 -0500

    Add files via upload
    wooooooo! completed my first iOS project today.

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Siddhant Singh on 5/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
